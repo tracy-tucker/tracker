5724b06088f28e12b405f874bfdf8224860522ed
treuther <tracyreuther@gmail.com>
2020-10-06 12:44:49 -0500

    nav notes added to component

--- components/nav.js
+++ components/nav.js
@@ -1,5 +1,11 @@
 import Link from 'next/link';
 
+//Nav setup:
+//Create a links array, that stores an object, which stores external link hrefs and link labels.
+//Map the array to assign link key
+//DOWN BELOW, map the links array again to list the links in an unordered list.
+// key is a required key
+
 const links = [
     {href: 'https://zeit.co/now', label: 'ZEIT'},
     {href: 'https://github.com/zeit/next.js', label: 'GITHUB'}
