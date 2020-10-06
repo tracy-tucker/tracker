951f896a6fe192d8558a445059ab2737e40861c5
treuther <tracyreuther@gmail.com>
2020-10-06 12:56:39 -0500

    router setup for {slug} using useRouter hook

--- pages/event/[slug].js
+++ pages/event/[slug].js
@@ -1,9 +1,14 @@
-import Layout from '../../components/Layout'
+import Layout from '../../components/Layout';
+import { useRouter } from 'next/router'; //NextJS hook
+
+//The page name has been changed to [slug].js to create a dynamic page
 
 const Event = () => {
+    const router = useRouter(); //assigned router to useRouter
+    const { slug } = router.query; //pulls in the dynamic slug
     return (
         <Layout>
-        <h1>Event</h1>
+        <h1>{slug}</h1> {/*Changed from Event to {slug}*/}
     </Layout>
     )
 }
