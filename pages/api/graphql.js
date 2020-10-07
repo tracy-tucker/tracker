41b5694b78ba3e43584b993c264323a80e398cba
treuther <tracyreuther@gmail.com>
2020-10-07 18:49:53 -0500

    response function with test object/value created

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,3 +1,13 @@
+//creating a new route
+
+import { redirect } from "next/dist/next-server/server/api-utils"
+
+//request comes in and returns a response
 export default (req, res) => { //both request and response are required
-    res.setHeader('Content-Type', 'application/json') //seHeader says what type of content we expect on the response.
+    //sending back a response to the client
+    res.setHeader('Content-Type', 'application/json') //seHeader tells the client what type of response this will be.
+    res.statusCode = 200 //200 means all good!
+    res.end(JSON.stringify ({ //converts the JS response object/value  to a JSON string
+        test: "Hello Level Up"
+    }))
 }
