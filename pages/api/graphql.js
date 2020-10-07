8e690b7260424e56fb357386084088737c27df5e
treuther <tracyreuther@gmail.com>
2020-10-07 13:00:51 -0500

    graphql.js created for API

---
+++ pages/api/graphql.js
@@ -0,0 +1,3 @@
+export default (req, res) => { //both request and response are required
+    res.setHeader('Content-Type', 'application/json') //seHeader says what type of content we expect on the response.
+}
