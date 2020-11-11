837246df208ac18b2857691bf0ec896f2e8c73e9
treuther <tracyreuther@gmail.com>
2020-11-11 03:37:26 -0600

    HabitButton added to Habit component

--- components/Habit.js
+++ components/Habit.js
@@ -1,15 +1,15 @@
+import HabitButton from './HabitButton'
+
 const Habit = () => {
     return (
         <article>
             <h3>Habit Title</h3>
             <div>
-                <button>O</button>
-                <button>O</button>
-                <button>O</button>
-                <button>X</button>
-                <button>O</button>
-                <button>X</button>
-                <button>O</button>
+                <HabitButton />
+                <HabitButton />
+                <HabitButton />
+                <HabitButton />
+                <HabitButton />
             </div>
         </article>
     )
