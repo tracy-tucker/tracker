e11e6f4a4adf00de3ab7505e215a39bfe73b07e3
treuther <tracyreuther@gmail.com>
2020-11-10 19:25:06 -0600

    basic Habit setup

---
+++ components/Habit.js
@@ -0,0 +1,18 @@
+const Habit = () => {
+    return (
+        <article>
+            <h3>Habit Title</h3>
+            <div>
+                <button>O</button>
+                <button>O</button>
+                <button>O</button>
+                <button>X</button>
+                <button>O</button>
+                <button>X</button>
+                <button>O</button>
+            </div>
+        </article>
+    )
+}
+
+export default Habit;
---
+++ components/HabitList.js
@@ -0,0 +1,1 @@
+import Habit from './Habit';
