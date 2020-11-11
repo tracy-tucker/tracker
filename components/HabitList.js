8f85747dc4fbce1010efd12f680803a3d61a8174
treuther <tracyreuther@gmail.com>
2020-11-11 03:20:32 -0600

    basic setup for HabitList

--- components/HabitList.js
+++ components/HabitList.js
@@ -1,1 +1,14 @@
 import Habit from './Habit';
+
+const HabitList = () => {
+    return (
+        <section>
+            <h2>MMy Habits</h2>
+            <Habit />
+            <Habit />
+            <Habit />
+        </section>
+    )
+}
+
+export default HabitList;
