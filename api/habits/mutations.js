11cb93998656b804663d0dd595daeb5d68f05e32
treuther <tracyreuther@gmail.com>
2020-11-13 04:29:32 -0600

    api/habits/mutations, resolvers, habits, Habit.graphql files created

---
+++ api/habits/mutations.js
@@ -0,0 +1,2 @@
+//This is where post requests happen
+//AKA, a REST server, IE "Add a habit"
---
+++ api/habits/resolvers.js
@@ -0,0 +1,2 @@
+//When we ask for data, this tells us WHAT data we will get back.
+//This is where we add our database calls and where we return our data.
