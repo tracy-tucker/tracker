aeeb75511729207a55603e5f6a9b476e2e3188bf
treuther <tracyreuther@gmail.com>
2020-11-13 04:42:44 -0600

    const for HabitsSchema

--- api/habits/habits.js
+++ api/habits/habits.js
@@ -1,1 +1,13 @@
-//This is where store our database collection.
+//This is where we store our database collection.
+
+import mongoose, { Schema } from 'mongoose';
+
+export const HabitsSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    }
+});
+
+export default mongoose.models.habits || mongoose.model('habits', HabitsSchema);
+//send changes to mongoose habits model OR create the model
