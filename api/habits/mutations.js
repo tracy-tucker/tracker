93514a3473acb9b52c117c0580a57bbafdc3976b
treuther <tracyreuther@gmail.com>
2020-11-16 03:57:48 -0600

    addHabit code added for addHabit mmutation

--- api/habits/mutations.js
+++ api/habits/mutations.js
@@ -1,2 +1,10 @@
 //This is where post requests happen
 //AKA, a REST server, IE "Add a habit"
+
+export const habitsMutations = {
+    Mutation: {
+        async addHabit(_, { habit }) {
+            console.log('add habit');
+        }
+    }
+}
