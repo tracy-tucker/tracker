439c20c8bb6528a92f142ef9087b6a6ff5476afe
treuther <tracyreuther@gmail.com>
2020-11-19 13:04:01 -0600

    updated HabitForm and mutations to send inputs to the database

--- api/habits/mutations.js
+++ api/habits/mutations.js
@@ -1,10 +1,18 @@
 //This is where post requests happen
 //AKA, a REST server, IE "Add a habit"
+import Habits from './habits'
 
 export const habitsMutations = {
     Mutation: {
         async addHabit(_, { habit }) {
-            console.log('add habit');
+            try {
+                const newHabit = await Habits.create({
+                    ...habit
+                })
+                return newHabit;
+            } catch (e) {
+                console.log(e)
+            }
         }
     }
 }
