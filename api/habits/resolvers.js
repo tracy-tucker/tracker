081bfc0da722db3e27387b24465610a683461447
treuther <tracyreuther@gmail.com>
2020-11-20 03:43:47 -0600

    updated resolvers to query the database for habits

--- api/habits/resolvers.js
+++ api/habits/resolvers.js
@@ -1,16 +1,17 @@
 //When we ask for data, this tells us WHAT data we will get back.
 //This is where we add our database calls and where we return our data.
 
+import Habits from './habits';
+
 export const habitsResolvers = {
-    Query: {
-      async habits() {
-        console.log('habits');
-        return [
-          {
-            _id: 'somefunkyarray',
-            name: 'Make my bed'
-          }
-        ];
+  Query: {
+    async habits() {
+      try {
+        const habits = await Habits.find();
+        return habits;
+      } catch (e) {
+        console.log('e', e);
       }
     }
-  };
+  }
+};
