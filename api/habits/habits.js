2800392a6bc010dec8eddd007c23a59cc987e751
treuther <tracyreuther@gmail.com>
2020-11-20 04:39:22 -0600

    added an EventsSchema to Habits to modify the Xs and Ox for each habit

--- api/habits/habits.js
+++ api/habits/habits.js
@@ -2,11 +2,20 @@
 
 import mongoose, { Schema } from 'mongoose';
 
+const EventsSchema = new Schema({
+    date: {
+        type: Date,
+        required: true,
+        unique: true //this prevents saying you completed the habit twice in one day
+    }
+});
+
 export const HabitsSchema = new Schema({
     name: {
         type: String,
         required: true
-    }
+    },
+    events: [EventsSchema]
 });
 
 export default mongoose.models.habits || mongoose.model('habits', HabitsSchema);
