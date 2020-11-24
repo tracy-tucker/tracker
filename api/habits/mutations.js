f5a61ec56f772f8f6a78690405ab8a1ca54572ae
treuther <tracyreuther@gmail.com>
2020-11-24 04:26:50 -0600

    updated mutation to include removeEvent

--- api/habits/mutations.js
+++ api/habits/mutations.js
@@ -16,14 +16,48 @@ export const habitsMutations = {
         },
         async addEvent(_, { habitId, date }) {
             try {
-                //
+
+                date.setHours(0,0,0,0);
+                const habit = await Habits.findOneAndUpdate(
+                    {
+                        _id: habitId,
+                        'events.date': {
+                            $ne: date //prevents duplicate dates from being saved to the DB
+                        }
+                    },
+                    {
+                        $addToSet: {
+                            events: {
+                                date
+                            }
+                        }
+                    }
+                );
+                return habit;
             } catch (e) {
                 console.log('e', e);
             }
         },
 
         async removeEvent(_, { habitId, eventId }) {
-            console.log('add event')
+            try {
+                
+                const habit = await Habits.findOneAndUpdate(
+                    {
+                        _id: habitId
+                    }, {
+                        $pull: {
+                            events: {
+                                _id: eventId
+                            }
+                        }
+                    }
+                );
+                return habit;
+
+            } catch (e) {
+                console.log('e', e)
+            }
         }
     }
 };
